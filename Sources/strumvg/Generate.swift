dc8202d0a951c70609a83923a316d3cd18911b79
Edon <edon@valdman.works>
2025-03-03 10:45:00 -0600

    Update Generate.swift
    Removed unused commented-out code (mostly direct from JS).

--- Sources/strumvg/Generate.swift
+++ Sources/strumvg/Generate.swift
@@ -20,7 +20,6 @@ extension strumvg {
         let allStrums = pattern.groups
             .flatMap(\.strums)
         
-//        const chars = createTaktChars(strums.length, noteLength);
         let strs = createRhythmText(quantity: allStrums.count, noteLength: pattern.timing)
         
         let calcWidth = (options.strumWidth + options.strumGap) * CGFloat(pattern.totalStrums) - options.strumGap
@@ -59,32 +58,6 @@ extension strumvg {
                         arrow,
                     ].compactMap { $0 }
                 )
-                
-                    
-//                let group = document.createElementNS("http://www.w3.org/2000/svg", "g");
-                
-//                group.setAttributeNS(null, "key", `strum\(i)`);
-//                group.style.transformBox = "fill-box";
-//                group.style.transformOrigin = "center";
-//                group.setAttributeNS(
-//                    null,
-//                    "transform",
-//                    `translate(\((_options.strumWidth + _options.strumGap) * i),${_options.headerHeight
-//                    })\(s.direction === "down" ? " rotate(180 0 0)" : "")`
-//                );
-                
-//                const arrow = createStrumArrow({
-//                    ...s,
-//                    height: _options.strumHeight,
-//                    width: _options.strumWidth,
-//                    fill: _options.arrowColor,
-//                });
-                
-//                if (arrow) {
-//                    group.appendChild(arrow);
-//                }
-                
-                return group
             }
         
         // MARK: Char StrumHeader
@@ -96,25 +69,10 @@ extension strumvg {
                     index: i,
                     rhythm: true
                 )
-//                return createStrumHeader(
-//                    {
-//                        key: `head${i}`,
-//                        height: _options.beatTextHeight,
-//                        width: _options.strumWidth,
-//                        fill: _options.rhythmColor,
-//                        fontSize: _options.beatTextFontSize,
-//                        y: _options.headerHeight + _options.strumHeight * FIX_FACTOR,
-//                    x:
-//                        (_options.strumWidth + _options.strumGap) * i +
-//                        _options.strumWidth / 2,
-//                    },
-//                    str
-//                );
             }
         
         let noteGroupsGroup = createNoteGroups(
             strums: allStrums,
-//            options: _options,
             noteLength: pattern.timing
         )
         
@@ -134,36 +92,6 @@ extension strumvg {
         
         let svg = SVG(
             svgAttrs: svgDeclAttrs,
-//            [
-//            [
-//                .attribute(named: "xmlns", value: "http://www.w3.org/2000/svg"),
-//                .attribute(named: "version", value: "1.1"),
-                
-//                .attribute(
-//                    named: "width",
-//    //                value: calcWidth
-//    //                      .formatted(numberFormat)
-//                    value: size
-//                        .map(\.width)
-//                        .map { $0.formatted(numberFormat) }
-//                ),
-//                .attribute(
-//                    named: "height",
-//    //                value: calcHeight
-//    //                      .formatted(numberFormat)
-//                    value: size
-//                        .flatMap { $0.height }
-//                        .map { $0.formatted(numberFormat) }
-//                ),
-//                .attribute(
-//                    named: "viewBox",
-//                    value: "0 0 \(calcWidth.formatted(numberFormat)) \(calcHeight.formatted(numberFormat))"
-//                ),
-//                .attribute(
-//                    named: "overflow",
-//                    value: "visible"
-//                )
-//            ],
             [
                 headers,
                 arrows,
@@ -172,10 +100,6 @@ extension strumvg {
                 [noteGroupsGroup],
             ].flatMap { $0 }
         )
-        
-//        svg.append(
-//            ...[...headers, ...arrows, ...charHeaders, noteGroups].flatMap((el) => el)
-//        );
         
         return svg
     }
@@ -280,25 +204,18 @@ extension strumvg {
         strum: Strum
     ) -> Node<SVG.DocumentContext>? {
         let variant = strum.variant
-        //   direction = "up",
         let height = options.strumHeight /*?? 100*/
         let width = options.strumWidth /*?? 50*/
         let fill = options.arrowColor
         let strokeWidth: CGFloat = 0.2
         let headHeight: CGFloat = 0.2
         
-//        let pathEl: SVGPathElement
         let pathEl: Node<SVG.DocumentContext>
-//        let gEl: SVGGElement
-        let gEl: Node<SVG.DocumentContext>
-//        let rectEl1: SVGRectElement
         let rectEl1: Node<SVG.DocumentContext>
-//        let rectEl2: SVGRectElement
         let rectEl2: Node<SVG.DocumentContext>
         
         switch variant {
         case .normal:
-//            pathEl = document.createElementNS("http://www.w3.org/2000/svg", "path");
             pathEl = Node<SVG.DocumentContext>.element(
                 named: "path",
                 attributes: [
@@ -312,19 +229,6 @@ extension strumvg {
                     )
                 ]
             )
-            
-//            pathEl.setAttributeNS(
-//                null,
-//                "d",
-//                `m0,\(height * headHeight)l\(width / 2),\(-height * headHeight)l${width / 2
-//                },\(height * headHeight)l\((-width * (1 - strokeWidth)) / 2),0l0,${height / 2
-//                }l\(-width * strokeWidth),0l0,\(-height / 2)l\(-width / 4),0z`
-//            );
-//            pathEl.setAttributeNS(null, "stroke-width", "0");
-            // ...props
-//            Object.keys(pathProps).forEach((key) => {
-//                pathEl.setAttributeNS(null, key, pathProps[key]?.toString());
-//            });
             
             return pathEl
             
@@ -346,22 +250,7 @@ extension strumvg {
                     .attribute(named: "stroke-width", value: "0")
                 ]
             )
-
-//            const pathEl1 = document.createElementNS(
-//                "http://www.w3.org/2000/svg",
-//                "path"
-//            );
-//            pathEl1.setAttributeNS(
-//                null,
-//                "d",
-//                "M\(0),\(height * headHeight)l\(width / 2),\(-height * headHeight)l\(width / 2),\(height * headHeight)"
-//            );
-//            pathEl1.setAttributeNS(null, "stroke-width", "0");
-            // ...props
-//            Object.keys(pathProps).forEach((key) => {
-//                pathEl1.setAttributeNS(null, key, pathProps[key]?.toString());
-//            });
-//            
+            
             // Squiggle
             let pathEl2 = Node<SVG.DocumentContext>.element(
                 named: "path",
@@ -384,40 +273,7 @@ extension strumvg {
                     .attribute(named: "stroke", value: fill /*?? ""*/)
                 ]
             )
-//            const pathEl2 = document.createElementNS(
-//                "http://www.w3.org/2000/svg",
-//                "path"
-//            );
-//            pathEl2.setAttributeNS(
-//                null,
-//                "d",
-//                `M\(width / 2) , \(offsetY / 2)l0 \(offsetY / 2)` +
-//                new Array(numWaves)
-//                    .fill(0)
-//                    .map(
-//                        (_, i) =>
-//                        `q\(i % 2 === 0 ? -amplitude : amplitude) ${wavelength / 2
-//                        } , \(0) \(wavelength)`
-//                    )
-//                    .join(", ")
-//            );
-//            pathEl2.setAttributeNS(
-//                null,
-//                "stroke-width",
-//                (width * strokeWidth).toString()
-//            );
-            // ...props (do it before the remaining attributes, as they need to override some of these properties)
-//            Object.keys(pathProps).forEach((key) => {
-//                pathEl2.setAttributeNS(null, key, pathProps[key]?.toString());
-//            });
-//            pathEl2.setAttributeNS(null, "fill", "transparent");
-//            pathEl2.setAttributeNS(null, "stroke", pathProps.fill ?? "");
-            
             gEl = Node<SVG.DocumentContext>.element(named: "g", nodes: [pathEl1, pathEl2])
-            
-//            gEl = document.createElementNS("http://www.w3.org/2000/svg", "g");
-//            gEl.appendChild(pathEl1);
-//            gEl.appendChild(pathEl2);
             return gEl
 
 //        case .accent:
@@ -433,18 +289,6 @@ extension strumvg {
 //                    .attribute(named: "stroke-width", value: "0")
 //                ]
 //            )
-////            pathEl = document.createElementNS("http://www.w3.org/2000/svg", "path");
-//            
-////            pathEl.setAttributeNS(
-////                null,
-////                "d",
-////                `m0,\(height * headHeight)l\(width / 2),\(-height * headHeight)l\(width / 2),\(height * headHeight)l\((-width * (1 - newStrokeWidth)) / 2),0l0,\(height / 2)l\(-width * newStrokeWidth),0l0,\(-height / 2)l\(-width / 4),0z`
-////            );
-////            pathEl.setAttributeNS(null, "stroke-width", "0");
-//            // ...props
-////            Object.keys(pathProps).forEach((key) => {
-////                pathEl.setAttributeNS(null, key, pathProps[key]?.toString());
-////            });
 //            
 //            return pathEl
             
@@ -460,19 +304,6 @@ extension strumvg {
                 ]
             )
 
-//            pathEl = document.createElementNS("http://www.w3.org/2000/svg", "path");
-            
-//            pathEl.setAttributeNS(
-//                null,
-//                "d",
-//                `m0,\(height * headHeight)l\(width),0l\((-width * (1 - strokeWidth)) / 2),0l0,\(height / 2)l\(-width * strokeWidth),0l0,\(-height / 2)l\(-width / 4),0z`
-//            );
-//            pathEl.setAttributeNS(null, "stroke-width", "0");
-//            // ...props
-//            Object.keys(pathProps).forEach((key) => {
-//                pathEl.setAttributeNS(null, key, pathProps[key]?.toString());
-//            });
-            
             let rectElAttrs: [Attribute<SVG.DocumentContext>] = [
                 .attribute(
                     named: "width",
@@ -495,7 +326,6 @@ extension strumvg {
                 ),
             ]
             
-//            rectEl1 = document.createElementNS("http://www.w3.org/2000/svg", "rect");
             rectEl1 = Node<SVG.DocumentContext>.element(
                 named: "rect",
                 attributes: [
@@ -503,25 +333,6 @@ extension strumvg {
                     [.attribute(named: "transform", value: "rotate(45 0 0)")]
                 ].flatMap { $0 }
             )
-            
-//            rectEl1.setAttributeNS(null, "width", (width * strokeWidth).toString());
-//            rectEl1.setAttributeNS(
-//                null,
-//                "height",
-//                (height * headHeight * 2).toString()
-//            );
-//            rectEl1.setAttributeNS(
-//                null,
-//                "x",
-//                (width / 2 - (width * strokeWidth) / 2).toString()
-//            );
-//            rectEl1.style.transformBox = "fill-box";
-//            rectEl1.style.transformOrigin = "center";
-//            rectEl1.setAttributeNS(null, "transform", "rotate(45 0 0)");
-//            // ...props
-//            Object.keys(pathProps).forEach((key) => {
-//                rectEl1.setAttributeNS(null, key, pathProps[key]?.toString());
-//            });
             
             rectEl2 = Node<SVG.DocumentContext>.element(
                 named: "rect",
@@ -531,9 +342,6 @@ extension strumvg {
                 ].flatMap { $0 }
             )
             
-//            rectEl2 = rectEl1.cloneNode() as SVGRectElement;
-//            rectEl2.setAttributeNS(null, "transform", "rotate(-45 0 0)");
-            
             gEl = Node<SVG.DocumentContext>.element(
                 named: "g",
                 nodes: [
@@ -542,10 +350,6 @@ extension strumvg {
                     rectEl2,
                 ]
             )
-//            gEl = document.createElementNS("http://www.w3.org/2000/svg", "g");
-//            gEl.appendChild(pathEl);
-//            gEl.appendChild(rectEl1);
-//            gEl.appendChild(rectEl2);
             return gEl
             
         case .space:
@@ -584,20 +388,6 @@ extension strumvg {
                 ].flatMap { $0 }
             )
             
-//            rectEl1 = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-//            rectEl1.setAttributeNS(
-//                null,
-//                "y",
-//                ((height / 4) * (1 - 1 / h_factor)).toString()
-//            );
-//            rectEl1.setAttributeNS(null, "x", (0 + width / 8).toString());
-//            rectEl1.setAttributeNS(null, "width", (width / 4).toString());
-//            rectEl1.setAttributeNS(null, "height", (height / h_factor).toString());
-//            // ...props
-//            Object.keys(pathProps).forEach((key) => {
-//                rectEl1.setAttributeNS(null, key, pathProps[key]?.toString());
-//            });
-            
             rectEl2 = Node<SVG.DocumentContext>.element(
                 named: "rect",
                 attributes: [
@@ -610,9 +400,6 @@ extension strumvg {
                 ].flatMap { $0 }
             )
             
-//            rectEl2 = rectEl1.cloneNode() as SVGRectElement;
-//            rectEl2.setAttributeNS(null, "x", (width / 2 + width / 8).toString());
-            
             gEl = Node<SVG.DocumentContext>.element(
                 named: "g",
                 nodes: [
@@ -621,16 +408,9 @@ extension strumvg {
                 ]
             )
             
-//            gEl = document.createElementNS("http://www.w3.org/2000/svg", "g");
-//            gEl.appendChild(rectEl1);
-//            gEl.appendChild(rectEl2);
             return gEl
             
         case .other(let char):
-//            const textEl = document.createElementNS(
-//                "http://www.w3.org/2000/svg",
-//                "text"
-//            );
             let textEl = Node<SVG.DocumentContext>.element(
                 named: "text",
                 nodes: [
@@ -679,24 +459,6 @@ extension strumvg {
                 ]
             )
             
-//            // ...props
-//            Object.keys(pathProps).forEach((key) => {
-//                rectEl1.setAttributeNS(null, key, pathProps[key]?.toString());
-//            });
-//            
-//            textEl.setAttributeNS(null, "y", (height / 2).toString());
-//            textEl.setAttributeNS(null, "font-size", (height / 2).toString());
-//            textEl.setAttributeNS(null, "x", (width / 2).toString());
-//            textEl.setAttributeNS(null, "text-anchor", "middle");
-//            textEl.setAttributeNS(null, "font-family", "sans-serif");
-//            textEl.setAttributeNS(null, "font-weight", "bold");
-//            textEl.setAttributeNS(null, "textLength", width.toString());
-//            textEl.setAttributeNS(null, "lengthAdjust", "spacingAndGlyphs");
-//            textEl.setAttributeNS(null, "width", width.toString());
-            
-//            const textContent = document.createTextNode(variant);
-//            textEl.appendChild(textContent);
-            
             return textEl
         }
     }
@@ -721,45 +483,13 @@ extension strumvg {
                 x: CGFloat(subdivision) * (options.strumWidth + options.strumGap) * CGFloat(i) + options.strumWidth / 2,
                 y: y,
                 width: CGFloat(subdivision) * (options.strumWidth + options.strumGap)
-                
-                //                    beamStrokeWidth: options.beamStrokeWidth,
-                //                    fill: options.rhythmColor,
-                //                    beamHeight: options.beamHeight,
-                //                    horizontalStrokes: horizontalStrokes,
-                //                    width: subdivision * (options.strumWidth + options.strumGap),
-                //                    y: y,
-                //                    x:
-                //                    subdivision * (options.strumWidth + options.strumGap) * i +
-                //                    options.strumWidth / 2,
-                //                    subtitle: triplet ? "3" : undefined,
             )
         }
         
-//        const gEl = document.createElementNS("http://www.w3.org/2000/svg", "g");
         return Node<SVG.DocumentContext>.element(
             named: "g",
             nodes: attrs
         )
-        
-//        const groups = new Array(quantity).fill(0).map((_, i) => {
-//            return createNoteGroup({
-//                quantity: subdivision,
-//                beamStrokeWidth: options.beamStrokeWidth,
-//                fill: options.rhythmColor,
-//                beamHeight: options.beamHeight,
-//                horizontalStrokes: horizontalStrokes,
-//                width: subdivision * (options.strumWidth + options.strumGap),
-//                y: y,
-//            x:
-//                subdivision * (options.strumWidth + options.strumGap) * i +
-//                options.strumWidth / 2,
-//                subtitle: triplet ? "3" : undefined,
-//            });
-//        });
-        
-//        gEl.append(...groups);
-        
-//        return gEl;
     }
     
     private func createNoteGroup(
@@ -796,22 +526,6 @@ extension strumvg {
                     .attribute(named: "fill", value: fill),
                 ]
             )
-            
-//            textEl.setAttributeNS(
-//                null,
-//                "x",
-//                ((width * (quantity - 1)) / quantity / 2).toString()
-//            );
-//            textEl.setAttributeNS(null, "y", (beamHeight + 16).toString());
-//            textEl.setAttributeNS(null, "font-size", "14");
-//            textEl.setAttributeNS(null, "text-anchor", "middle");
-//            textEl.setAttributeNS(null, "font-family", "sans-serif");
-//            textEl.setAttributeNS(null, "fill", fill);
-            
-//            const textContent = document.createTextNode(subtitle);
-//            textEl.appendChild(textContent);
-            
-//            gEl.appendChild(textEl);
         } else {
             textEl = nil
         }
