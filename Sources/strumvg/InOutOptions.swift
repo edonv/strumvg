56ddc53d69388a369da2b6cc5423a01a63f7cb87
Edon <edon@valdman.works>
2025-03-05 09:53:46 -0600

    Create InOutOptions.swift
    Added new `InOutOptions` type that conforms to `ParsableArguments`. This is used to allow changing the input/output source/destination.

---
+++ Sources/strumvg/InOutOptions.swift
@@ -0,0 +1,45 @@
+//
+//  InOutOptions.swift
+//  strumvg
+//
+//  Created by Edon Valdman on 3/3/25.
+//
+
+import Foundation
+import ArgumentParser
+
+struct InOutOptions: ParsableArguments {
+    @Flag(help: "Source for input pattern string.")
+    var inputSource: InputSource = .argument
+    
+    @Flag(help: "Destination for output SVG content.")
+    var outputDestination: OutputDestination = .log
+    
+    enum InputSource: EnumerableFlag {
+        case stdin
+        case argument
+        
+        static func name(for value: InputSource) -> NameSpecification {
+            switch value {
+            case .stdin:
+                return [.customShort("s"), .long]
+            case .argument:
+                return [.customShort("a"), .customLong("arg")]
+            }
+        }
+    }
+    
+    enum OutputDestination: EnumerableFlag {
+        case stdout
+        case log
+        
+        static func name(for value: OutputDestination) -> NameSpecification {
+            switch value {
+            case .stdout:
+                return [.customShort("s"), .long]
+            case .log:
+                return [.customShort("l"), .long]
+            }
+        }
+    }
+}
