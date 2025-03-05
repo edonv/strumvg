ec3568066e798cd76f0773121faf3f32529d227a
Edon <edon@valdman.works>
2025-03-05 09:54:51 -0600

    Update strumvg.swift
    Implemented different input/output from `ioOptions`.

--- Sources/strumvg/strumvg.swift
+++ Sources/strumvg/strumvg.swift
@@ -4,6 +4,7 @@
 // Swift Argument Parser
 // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
 
+import Foundation
 import ArgumentParser
 import StrumModels
 
@@ -33,13 +34,50 @@ struct strumvg: ParsableCommand {
     }
     
     mutating func run() throws {
+        let str: String
+        
+        switch ioOptions.inputSource {
+        case .stdin:
+            let stdin = FileHandle.standardInput
+            guard let data = try stdin.readToEnd() else {
+                print("Failed to read all incoming data from stdin.")
+                throw ExitCode(EXIT_FAILURE)
+            }
+            guard let dataStr = String(data: data, encoding: .utf8) else {
+                print("Failed to convert stdin data to UTF-8 string.")
+                throw ExitCode(EXIT_FAILURE)
+            }
+            str = dataStr
+            
+        case .argument:
+            guard let patternString else {
+                throw ValidationError("`inputSource` flag set to `--arg` and the `patternString` argument is missing.")
+            }
+            
+            str = patternString
+        }
+        
 //        let string = "{xD} D u uD u-16t"
-        let pattern = Pattern(rawValue: patternString)
+        let pattern = Pattern(rawValue: str)
 //        print(pattern?.rawValue)
         
         guard let pattern else { return }
         
         let svg = generate(pattern: pattern)
-        print(svg.render(indentedBy: .spaces(2)))
+        let svgStr = svg.render(indentedBy: .spaces(2))
+        
+        switch ioOptions.outputDestination {
+        case .stdout:
+            guard let svgStrData = svgStr.data(using: .utf8) else {
+                print("Failed to convert SVG string content to UTF-8 data.")
+                throw ExitCode(EXIT_FAILURE)
+            }
+            
+            let stdout = FileHandle.standardOutput
+            try stdout.write(contentsOf: svgStrData)
+            
+        case .log:
+            print(svgStr)
+        }
     }
 }
