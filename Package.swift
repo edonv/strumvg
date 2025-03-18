9dc87df9829e945e4c7222831c80bd1452c83fc1
Edon <edon@valdman.works>
2025-03-18 10:14:00 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-        .package(url: "https://github.com/edonv/PlotSVG.git", from: "0.0.0"),
         .package(url: "https://github.com/JohnSundell/Plot.git", from: "0.14.0"),
+        .package(url: "https://github.com/edonv/PlotSVG.git", from: "0.0.0"),
     ],
     targets: [
         .target(name: "StrumModels"),
