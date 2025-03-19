add44bed6f754fd16293704008caf5bce3891466
Edon <edon@valdman.works>
2025-03-19 10:23:10 -0500

    Update strumvg.swift
    Updated file version.

--- Sources/strumvg/strumvg.swift
+++ Sources/strumvg/strumvg.swift
@@ -16,7 +16,7 @@ struct strumvg: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "A command for generating an SVG of a strumming pattern.",
         discussion: "Any SVG-compatible value can be used for any configuration option.",
-        version: "1.0.3"
+        version: "1.1.0"
     )
     
     @Argument(help: "The string representation of a pattern.")
