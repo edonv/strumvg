3c7f5abb8d17ad1bc60bc949e6fa8c941d8638ec
Edon <edon@valdman.works>
2026-06-16 08:44:48 -0500

    Update Package.swift
    Upped minimum macOS version to 15.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "strumvg",
     platforms: [
-        .macOS(.v13),
+        .macOS(.v15),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
