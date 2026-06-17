2a9f4b9f2aefb722ed5f8a0a3a2f8ec8e34f8af1
Edon <edon@valdman.works>
2026-06-17 06:57:56 -0500

    Update Package.swift
    Added new target for configuration types.

--- Package.swift
+++ Package.swift
@@ -26,6 +26,12 @@ let package = Package(
                 .product(name: "Plot", package: "Plot"),
             ]
         ),
+        .target(
+            name: "StrumVGConfig",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
