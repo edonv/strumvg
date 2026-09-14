0dd977ca64b88907350eb8af8764e8294cc23eb7
Edon <edon@valdman.works>
2026-09-14 13:39:17 -0500

    Update Pattern.swift
    Removed `RawRepresentable` conformance.

--- Sources/StrumModels/Pattern.swift
+++ Sources/StrumModels/Pattern.swift
@@ -13,64 +13,11 @@ import RegexBuilder
 /// Raw value: `[measure]+` (separated by `"|"`), each measure can end with `-[timing]`
 ///
 /// Can also have a timing identifier at the end of the full string to represent for the full pattern.
-public struct Pattern: RawRepresentable, Sendable, Hashable {
+public struct Pattern: Sendable, Hashable {
     public let measures: [Measure]
     
     public init(measures: [Measure]) {
         self.measures = measures
-    }
-    
-    /// - Returns: A validated `Pattern`, or `nil` if the format is invalid.
-    public init?(rawValue: String) {
-        let timingRegexMatches = rawValue.matches(of: Timing.regex)
-        
-        guard !timingRegexMatches.isEmpty else { return nil }
-        
-        var rhythmGroupsByMeasure = rawValue
-            // Trim only newlines in case there is intentional leading whitespace in the strums
-            .trimmingCharacters(in: .newlines)
-            .split(separator: "|")
-            // Initial clean-up
-            .map { measureStr in
-                measureStr
-                    // Trim only newlines in case there is intentional leading whitespace in the strums
-                    .trimmingCharacters(in: .newlines)
-                    // Remove any remaining barlines in each measure
-                    .replacingOccurrences(of: "|", with: "")
-            }
-        
-        // if the Timing segment is separated by a barline and there is either:
-        // - only 1 measure
-        // - more than 1 measure but only 1 Timing segment
-        if timingRegexMatches.count == 1
-            && rhythmGroupsByMeasure.count > 1,
-           let firstMatch = timingRegexMatches.first,
-           let timing = Timing(rawValue: String(firstMatch.output.0)) {
-            // remove only timing string
-            if let index = rhythmGroupsByMeasure.firstIndex(of: timing.rawValue) {
-                rhythmGroupsByMeasure.remove(at: index)
-            }
-            
-            // append each measure string with the timing string
-            rhythmGroupsByMeasure = rhythmGroupsByMeasure
-                .map { segment in
-                    guard !segment.contains(timing.rawValue) else { return segment }
-                    return segment + timing.rawValue
-                }
-        }
-        
-        self.measures = rhythmGroupsByMeasure
-//            .compactMap(Measure.init(rawValue:))
-            .map { Measure(rawValue: $0)! }
-        
-        // validation that for every repeatStart theres a repeatEnd
-        guard self.validateRepeats() else { return nil }
-    }
-    
-    public var rawValue: String {
-        measures
-            .map(\.rawValue)
-            .joined(separator: "|")
     }
     
     /// Validates that for every ``repeatStart`` theres a ``repeatEnd``.
