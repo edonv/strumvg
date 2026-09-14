3792f0649fb9625de482aabc58004eb867a62d8f
Edon <edon@valdman.works>
2026-09-14 13:36:28 -0500

    Update Measure.swift
    Removed `RawRepresentable` conformance.

--- Sources/StrumModels/Measure.swift
+++ Sources/StrumModels/Measure.swift
@@ -11,7 +11,7 @@ import RegexBuilder
 /// A measure (or bar) of strumming.
 ///
 /// Raw value: `[pattern]-[noteLength]`
-public struct Measure: RawRepresentable, Sendable, Hashable {
+public struct Measure: Sendable, Hashable {
     /// An array of rhythmic groupings.
     public let groups: [RhythmicGroup]
     /// A specification describing how the groups' timings should be grouped.
@@ -72,77 +72,5 @@ public struct Measure: RawRepresentable, Sendable, Hashable {
         self.timing = timing
         self.repeatStart = repeatStart
         self.repeatEnd = repeatEnd
-    }
-    
-    /// - Returns: A validated `Measure`, or `nil` if the `timing` component is missing.
-    public init?(rawValue: String) {
-        // Timing
-        
-        guard let timing = Timing(rawValue: rawValue) else { return nil }
-        
-        // Strums
-        
-        /// Each strum element on their own lines
-        let groupStrumsByRhythm = rawValue
-            // Trim only newlines in case there is intentional leading whitespace in the strums
-            .trimmingCharacters(in: .newlines)
-            // remove repeat marks
-            .replacingOccurrences(of: ":", with: "")
-            // Regex for whatever `timing` is
-            .replacing(timing.rhythmicGroupingRegex) { match in
-                "\(match.output.1)\n"
-            }
-            // remove original trailing noteLength
-            .replacing {
-                "\n"
-                Timing.regex
-                Anchor.endOfSubject
-            } with: { _ in "" }
-            // group all individual strum chars in curly braces
-            .replacing(/([^\{\}\n])(?![^\{\}\n]?\})(?=.*$)/.anchorsMatchLineEndings()) { match in
-                return "{\(match.output.1)}"
-            }
-            // Trim only newlines in case there is intentional leading whitespace in the strums
-            .trimmingCharacters(in: .newlines)
-        
-//        print(groupStrumsByRhythm)
-        
-        // Split by inserted new lines to get each group separately
-        var groupsTemp = groupStrumsByRhythm
-            .components(separatedBy: "\n")
-            // .components(separatedBy:) can result in empty items
-            .filter { !$0.isEmpty }
-            .compactMap(RhythmicGroup.init(rawValue:))
-        
-        // If the measure doesn't have the correct number of strums for the appropriate `timing`,
-        // add extra spaces to fill it out
-        if !groupsTemp.isEmpty,
-           let lastGroup = groupsTemp.last,
-           lastGroup.strums.count < timing.stemsPerGroup {
-            groupsTemp[groupsTemp.count - 1] = lastGroup.appending(
-                strums: .init(
-                    repeating: .init(kind: .space),
-                    count: timing.stemsPerGroup - lastGroup.strums.count
-                )
-            )
-        }
-        
-        let strWithoutTiming = rawValue.replacing(Timing.regex, with: "")
-        
-        self.init(
-            groups: groupsTemp,
-            timing: timing,
-            // Check for repeats
-            repeatStart: strWithoutTiming.contains(/^:/),
-            repeatEnd: strWithoutTiming.contains(/:$/)
-        )
-    }
-    
-    public var rawValue: String {
-        groups
-            .flatMap(\.strums)
-            .map(\.rawValue)
-            .joined()
-        + "\(timing.rawValue)"
     }
 }
