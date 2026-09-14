738b7a3e5434dd1da50a87bf905caa99f7561bb4
Edon <edon@valdman.works>
2026-09-14 13:30:29 -0500

    Update Timing.swift
    Removed `RawRepresentable` conformance. Also removed regex-related code.

--- Sources/StrumModels/Timing.swift
+++ Sources/StrumModels/Timing.swift
@@ -9,7 +9,7 @@ import Foundation
 import RegexBuilder
 import Parsing
 
-public struct Timing: RawRepresentable, Sendable, Hashable {
+public struct Timing: Sendable, Hashable {
     public let duration: NoteDuration
     public let triplet: Bool
     
@@ -18,20 +18,6 @@ public struct Timing: RawRepresentable, Sendable, Hashable {
         self.triplet = triplet
     }
     
-    /// `rawValue` can include other content, but will initialize from first match of a valid pattern.
-    public init?(rawValue: String) {
-        let timingRegex = Timing.regex
-        
-        guard let timingMatch = try? timingRegex.firstMatch(in: rawValue)?.output,
-              let durationInt = Int(timingMatch.time),
-              let duration = NoteDuration(rawValue: durationInt) else { return nil }
-        
-        self.init(duration: duration, triplet: timingMatch.triplet != nil)
-    }
-    
-    public var rawValue: String {
-        "-\(duration.rawValue)\(triplet ? "t" : "")"
-    }
     
     /// The (maximum) number of note stems per group, depending on the type of `Timing`.
     public var stemsPerGroup: Int {
@@ -64,50 +50,5 @@ public struct Timing: RawRepresentable, Sendable, Hashable {
                 })
         }
         .eraseToAnyParserPrinter()
-    }
-    
-    /// `/-(?<time>4|8|16)(?<triplet>t)?/`
-    internal static var regex: Regex<(Substring, time: Substring, triplet: Substring?)> {
-        /-(?<time>4|8|16)(?<triplet>t)?/
-    }
-    
-    internal var rhythmGroupingRegexCountRange: ClosedRange<Int> {
-        switch self.triplet {
-        case true:
-            1...stemsPerGroup
-        case false:
-            switch duration {
-            case .quarter: 1...1
-            case .eighth: 1...2
-            case .sixteenth: 1...4
-            }
-        }
-    }
-    
-    internal var rhythmicGroupingRegex: Regex<(Substring, Substring)> {
-        Regex {
-            Capture {
-                Repeat(self.rhythmGroupingRegexCountRange) {
-                    ChoiceOf {
-                        CharacterClass.anyOf("{}")
-                            .inverted
-                        Regex {
-                            "{"
-                            Repeat(CharacterClass.anyNonNewline, 1...2)
-                            "}"
-                        }
-                    }
-                }
-            }
-            
-            Lookahead {
-                // /.*-4$/
-                ZeroOrMore(CharacterClass.anyNonNewline)
-                
-                self.rawValue
-                
-                Anchor.endOfSubject
-            }
-        }
     }
 }
