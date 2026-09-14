6a6e3c6d48cfc7e25232af5b469a125dbca5f36b
Edon <edon@valdman.works>
2026-09-14 13:34:17 -0500

    Update Measure.swift
    Added docs to primary init.

--- Sources/StrumModels/Measure.swift
+++ Sources/StrumModels/Measure.swift
@@ -38,6 +38,16 @@ public struct Measure: RawRepresentable, Sendable, Hashable {
         self.repeatEnd = repeatEnd
     }
     
+    /// Create a `Measure` of strums.
+    ///
+    /// Internally, `strums` will be split into groups according to `timing` and its ``Timing/subdivision``.
+    ///
+    /// Additionally, if `strums` doesn't have enough strums to make a full final rhythmic group of subdivisions, the final group will be padded at the end with empty ``StrumKind/space`` strums.
+    /// - Parameters:
+    ///   - strums: The strums to include in the measure.
+    ///   - timing: The timing for laying out the strums.
+    ///   - repeatStart: If the measure should include the start of a repeat.
+    ///   - repeatEnd: If the measure should include the end of a repeat.
     public init(
         strums: [Strum],
         timing: Timing,
